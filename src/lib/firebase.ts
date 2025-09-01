b1765f9588300c5f0d5877e198f2c8ff5dacfd2b
Rohit Prasad ch <RohitChigatapu@gmail.com>
2025-09-01 18:57:12 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  "projectId": "medreport-clarity",
+  "appId": "1:775990169875:web:f615cdbb6c03bd228d4ae6",
+  "storageBucket": "medreport-clarity.firebasestorage.app",
+  "apiKey": "AIzaSyDUmevArXKdJHLcJf3ShhmXEAMtRn72cTQ",
+  "authDomain": "medreport-clarity.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "775990169875"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
