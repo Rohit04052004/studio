d474e585885d4bfa5db6246cf2826a92355d40c6
Rohit Prasad ch <RohitChigatapu@gmail.com>
2025-09-01 18:50:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,15 +1,18 @@
 'use client';
+import { SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from './sidebar';
 import { Header } from './header';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen w-full">
-      <AppSidebar />
-      <div className="flex flex-col flex-1">
-        <Header />
-        <main className="flex-1 p-4 sm:p-6 lg:p-8">{children}</main>
+    <SidebarProvider>
+      <div className="flex min-h-screen w-full">
+        <AppSidebar />
+        <div className="flex flex-col flex-1">
+          <Header />
+          <main className="flex-1 p-4 sm:p-6 lg:p-8">{children}</main>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
