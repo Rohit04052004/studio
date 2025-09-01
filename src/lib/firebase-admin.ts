bede3c79bb3709c27a82da83a660fc2a00c377da
Rohit Prasad ch <RohitChigatapu@gmail.com>
2025-09-01 21:11:07 +0000

    just reminding ; var admin = require("firebase-admin");
    var serviceAcco

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,28 +1,20 @@
 
 import * as admin from 'firebase-admin';
+import serviceAccount from '../../../serviceAccountKey.json';
 
 let db: admin.firestore.Firestore | null = null;
 let auth: admin.auth.Auth | null = null;
 
 if (!admin.apps.length) {
-  const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
-
-  if (serviceAccountString) {
-    try {
-      const decodedServiceAccount = Buffer.from(serviceAccountString, 'base64').toString('utf-8');
-      const serviceAccount = JSON.parse(decodedServiceAccount);
-
-      admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-      });
-      console.log('Firebase Admin SDK initialized successfully.');
-      db = admin.firestore();
-      auth = admin.auth();
-    } catch (error: any) {
-      console.error('Firebase Admin SDK initialization error:', error.message);
-    }
-  } else {
-    console.error('FIREBASE_SERVICE_ACCOUNT_BASE64 environment variable is not set.');
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+    console.log('Firebase Admin SDK initialized successfully.');
+    db = admin.firestore();
+    auth = admin.auth();
+  } catch (error: any) {
+    console.error('Firebase Admin SDK initialization error:', error.message);
   }
 } else {
   db = admin.firestore();
