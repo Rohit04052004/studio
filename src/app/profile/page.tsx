d4ce2979b2a6c65f427b89831adfaeeb5676350b
Rohit Prasad ch <RohitChigatapu@gmail.com>
2025-09-01 18:36:08 +0000

    i want u to totally remove the profile setting page we will develope it

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,18 +1,5 @@
 import { ProfileClient } from '@/components/profile/profile-client';
 
 export default function ProfilePage() {
-  const userData = {
-    fullName: 'rohit chigatapu',
-    email: 'rohitchigatapu@example.com',
-    avatarUrl: 'https://picsum.photos/100',
-    memberSince: 'Jan 2024',
-    reportsAnalyzed: 0,
-    questionsAsked: 0,
-    aiPreferences: {
-      language: 'en',
-      tone: 'professional',
-    },
-  };
-
-  return <ProfileClient user={userData} />;
+  return <ProfileClient />;
 }
