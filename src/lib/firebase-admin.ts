214e978e1604f3772d52a7c143d9c357dab08358
Rohit Prasad ch <RohitChigatapu@gmail.com>
2025-09-01 21:49:39 +0000

    var admin = require("firebase-admin");
    var serviceAccount = require("pa

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,21 +1,14 @@
 
 import * as admin from 'firebase-admin';
+import serviceAccount from '../../serviceAccountKey.json';
 
 let db: admin.firestore.Firestore | null = null;
 let auth: admin.auth.Auth | null = null;
 
 if (!admin.apps.length) {
   try {
-    const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
-    if (!serviceAccountString) {
-      throw new Error('FIREBASE_SERVICE_ACCOUNT_BASE64 environment variable is not set.');
-    }
-    
-    const decodedServiceAccount = Buffer.from(serviceAccountString, 'base64').toString('utf8');
-    const serviceAccount = JSON.parse(decodedServiceAccount);
-
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     });
     db = admin.firestore();
     auth = admin.auth();
