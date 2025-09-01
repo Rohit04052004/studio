fb064a1169077fee455e8a26e147ae5218875ac5
Rohit Prasad ch <RohitChigatapu@gmail.com>
2025-09-01 21:52:28 +0000

    File changes

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,14 +1,15 @@
 
 import * as admin from 'firebase-admin';
-import serviceAccount from '../../serviceAccountKey.json';
 
 let db: admin.firestore.Firestore | null = null;
 let auth: admin.auth.Auth | null = null;
 
 if (!admin.apps.length) {
   try {
+    // When deployed to App Hosting, the GOOGLE_APPLICATION_CREDENTIALS environment variable is automatically set.
+    // When running locally, this is set in the .env.local file.
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+        credential: admin.credential.applicationDefault(),
     });
     db = admin.firestore();
     auth = admin.auth();
@@ -16,8 +17,8 @@ if (!admin.apps.length) {
     console.error('Firebase Admin SDK initialization error:', error.message);
   }
 } else {
-  db = admin.firestore();
-  auth = admin.auth();
+  db = admin.app().firestore();
+  auth = admin.app().auth();
 }
 
 export { db, auth };
