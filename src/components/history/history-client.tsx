eb6598533e527763f23cf1b9b22d023dda00bf97
Rohit Prasad ch <RohitChigatapu@gmail.com>
2025-09-23 10:25:24 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/history/history-client.tsx
+++ src/components/history/history-client.tsx
@@ -316,7 +316,7 @@ function ReportHistoryItem({ report, onCardClick, onDelete, isPending }: { repor
                     </AlertDialogContent>
                 </AlertDialog>
             </CardFooter>
-        </Card>>
+        </Card>
     )
 }
 
